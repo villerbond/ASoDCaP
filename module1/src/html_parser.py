30cd38325f53f0732794e97408741b16faf0e917
villerbond <v1llerbond@yandex.ru>
2026-03-12 21:59:32 +0300

    Added the first version of parser manager

---
+++ module1/src/html_parser.py
@@ -0,0 +1,6 @@
+class HTMLParser:
+    def __init__(self):
+        pass
+
+    def parse():
+        pass
--- module1/src/parser_manager.py
+++ module1/src/parser_manager.py
@@ -1,8 +1,21 @@
+from .html_parser import HTMLParser
+
 class ParserManager:
-    def __init__(self, parser):
-        self.parser = parser
-    def process_file(self, file):
-        with open(file, "r", encoding="utf-8") as f:
-            html = f.read()
+
+    def __init__(self):
+        self.parser = HTMLParser()
+
+    def process_file(self, file: str):
+
+        try:
+            with open(file, "r", encoding="utf-8") as f:
+                html = f.read()
+        except FileNotFoundError:
+            print(f"Error: file '{file}' not found")
+            return None
+        except Exception as e:
+            print(f"Error with reading file: {e}")
+            return None
+        
         result = self.parser.parse(html)
         return result
