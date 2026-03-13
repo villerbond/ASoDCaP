8912a4d3cc3ee4a3e83c6279c99114fbefaa965c
villerbond <v1llerbond@yandex.ru>
2026-03-13 13:29:51 +0300

    Added saving to json and csv

--- module1/src/parser_manager.py
+++ module1/src/parser_manager.py
@@ -1,9 +1,13 @@
 from .html_parser import HTMLParser
 from schemas.schemas import product_schema
+import os
+import json
+import csv
 
 class ParserManager:
 
     def __init__(self, parser_type: str = "html.parser"):
+        
         self.parser = HTMLParser(parser_type)
         self.schemas = [("products", product_schema)]
 
@@ -21,3 +25,33 @@ class ParserManager:
         
         result = self.parser.parse(html, self.schemas)
         return result
+    
+
+    def process_directory(self, directory: str):
+        results = []
+
+        for file in os.listdir(directory):
+            if file.endswith(".html"):
+                path = os.path.join(directory, file)
+                result = self.process_file(path)
+                if result:
+                    results.append(result)
+                    
+        return results
+    
+    def save_json(self, data, path):
+
+        with open(path, "w", encoding="utf-8") as f:
+            json.dump(data, f, indent=4)
+
+    def save_csv(self, data, path):
+
+        if not data:
+            return
+        
+        keys = data[0].keys()
+
+        with open(path, "w", newline="", encoding="utf-8") as f:
+            writer = csv.DictWriter(f, fieldnames=keys)
+            writer.writeheader()
+            writer.writerows(data)
