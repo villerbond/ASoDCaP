60dd6552c105d71b049b7318043359529f3970a8
villerbond <v1llerbond@yandex.ru>
2026-03-13 16:06:10 +0300

    Updated main.py, added argparser and commands

--- module1/main.py
+++ module1/main.py
@@ -1,13 +1,40 @@
+import argparse
 from src.parser_manager import ParserManager
-import json
 
 def main():
-    html_file = "data/sample.html"
-    manager = ParserManager(parser_type="lxml")
-    result = manager.process_file(html_file)
-    print(result)
-    with open("output/result.json", "w", encoding="utf-8") as f:
-        json.dump(result, f, indent=4, ensure_ascii=False)
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("path", help="Path to HTML/XML file or directory")
+    parser.add_argument("--format", choices=["json", "csv"], help="Export format")
+    parser.add_argument("--visualize", action="store_true",help="Visualize DOM tree")
+
+    args = parser.parse_args()
+
+    manager = ParserManager()
+
+    if args.path.endswith(".html") or args.path.endswith(".xml"):
+        result = manager.process_file(args.path)
+    else:
+        result = manager.process_directory(args.path)
+
+    if not result:
+        return
+    
+    if args.visualize:
+        if isinstance(result, dict) and 'dom_tree' in result:
+            print("\nDOM TREE OF DOCUMENT:\n")
+            manager.html_parser.visualize_dom_tree(result["dom_tree"])
+        else:
+            print("DOM visualization available only from HTML documents")
+
+    if args.format == "json":
+        manager.save_json(result, "output/result.json")
+    elif args.format == "csv":
+        if isinstance(result, dict) and "products" in result:
+            manager.save_csv(result["products"], "output/products.csv")
+        else:
+            print("CSV export only for structured data")
 
 if __name__ == "__main__":
     main()
